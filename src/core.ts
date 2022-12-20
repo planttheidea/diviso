91cfae16c70f237cc9f22d0bf0444224a533074d
Tony Quetano <tony.quetano@planttheidea.com>
2022-12-20 12:11:02 -0500

    include types in core file

--- src/core.ts
+++ src/core.ts
@@ -1,3 +1,47 @@
 export { compose } from './core/compose';
 export { createStore } from './core/store';
 export { part } from './core/part';
+
+export type {
+  Action,
+  AnyAction,
+  PartAction,
+  ActionCreator,
+} from './core/actions';
+export type { PromiseCacheEntry } from './core/async';
+export type { DevToolsEnhancerOptions as DevToolsOptions } from './core/devtools';
+export type {
+  AnyComposedPart,
+  AnyPart,
+  AnyStatefulPart,
+  AnySelectPart,
+  AnySelectablePart,
+  AnyUpdatePart,
+  AnyUpdateablePart,
+} from './core/part';
+export type {
+  Reducer,
+  ReducersMapObject,
+  StateFromReducersMapObject,
+  ReducerFromReducersMapObject,
+  ActionFromReducer,
+  ActionFromReducersMapObject,
+} from './core/reducer';
+export type {
+  Dispatch,
+  Listener,
+  Middleware,
+  MiddlewareAPI,
+  Notifier,
+  Notify,
+  Observable,
+  Observer,
+  PreloadedState,
+  DivisoStore as Store,
+  StoreCreator,
+  StoreEnhancer,
+  StoreEnhancerStoreCreator,
+  Subscribe,
+  SubscribeToPart,
+  Unsubscribe,
+} from './core/store';
